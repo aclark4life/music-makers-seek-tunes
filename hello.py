617fb17e9a21c34e628a9dc16cc72c3ee7f22f92
Alex Clark <aclark@aclark.net>
2021-11-25 13:57:26 -0500

    Update

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ import os
 
 consumer_key = os.environ.get("TWITTER_API_KEY")
 consumer_secret = os.environ.get("TWITTER_API_KEY_SECRET")
+access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
