9cbdf4f88979b96b5604c827b0bec6b8eeb79725
Alex Clark <aclark@aclark.net>
2021-11-25 11:22:31 -0500

    Update

---
+++ music-makers-seek-tunes.py
@@ -0,0 +1,30 @@
+# https://stackoverflow.com/a/55804977
+
+user_name = "@nameofuser"
+
+replies = tweepy.Cursor(
+    api.search, q="to:{}".format(user_name), since_id=tweet_id, tweet_mode="extended"
+).items()
+while True:
+    try:
+        reply = replies.next()
+        if not hasattr(reply, "in_reply_to_status_id_str"):
+            continue
+        if reply.in_reply_to_status_id == tweet_id:
+            logging.info("reply of tweet:{}".format(reply.full_text))
+
+    except tweepy.RateLimitError as e:
+        logging.error("Twitter api rate limit reached".format(e))
+        time.sleep(60)
+        continue
+
+    except tweepy.TweepError as e:
+        logging.error("Tweepy error occured:{}".format(e))
+        break
+
+    except StopIteration:
+        break
+
+    except Exception as e:
+        logger.error("Failed while fetching replies {}".format(e))
+        break
